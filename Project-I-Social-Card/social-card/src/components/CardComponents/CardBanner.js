11c7b5a9688cbe4fb051594178ea735a3e58710b
slaforsjov <andrewj12763@gmail.com>
2019-05-29 19:02:50 -0600

    finishing

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+const CardBanner = () => {
+    return (
+
+        <div className="card-banner" />
+
+    );
+  };
+  
+  export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div className="headertitle">
+            <h1 className="title">Lambda School</h1>
+            <p className="date">@lambdaschool . 26 Jan</p>
+        </div>
+    );
+  };
+
+export default HeaderTitle;
