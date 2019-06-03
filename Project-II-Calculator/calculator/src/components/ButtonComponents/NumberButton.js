dd028fdf5fef1697ab7834b9c068f6505d29c9b0
slaforsjov <andrewj12763@gmail.com>
2019-06-02 19:26:19 -0600

    calculator assinment

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,24 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = () => {
+
+    return (
+        <div className="numberPad">
+            <div className="numberButton" id="clear">clear</div>
+            <div className="numberButton">7</div>
+            <div className="numberButton">8</div>
+            <div className="numberButton">9</div>
+            <div className="numberButton">4</div>
+            <div className="numberButton">5</div>
+            <div className="numberButton">6</div>
+            <div className="numberButton">1</div>
+            <div className="numberButton">2</div>
+            <div className="numberButton">3</div>
+            <div className="numberButton" id="zero">0</div>
+        </div>
+    );
+}
+
+
+export default NumberButton;
